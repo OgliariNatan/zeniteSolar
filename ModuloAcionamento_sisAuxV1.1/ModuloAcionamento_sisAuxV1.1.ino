425f8fac325c94089ddf7518244f31446fc3303c
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2016-09-10 11:08:46 -0300

    Arduino
    Atualizado

---
+++ ModuloAcionamento_sisAuxV1.1/ModuloAcionamento_sisAuxV1.1.ino
@@ -0,0 +1,72 @@
+/*----------------------------------------------
+--Modulo Acionamento dos Sistemas auxiliares----
+--Autor Natan Ogliari --------------------------
+--Equipe Zênite---------------------------------
+--PD6; PB0; PA0---------------------------------
+*/
+#include "CAN_BUS_Shield/mcp_can.h"
+#include <SPI.h>
+//#include   avr/wdt.h //biblioteca do Watchdog
+
+unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+
+#define BOMBA1  6      //seta pino 6 (PD6) como BOMBA 1
+#define BOMBA2  8      //seta pino 8(PB0) como BOMBA 2
+#define INTER   A0      //seta A0 (PA0), como INTERrupção do mcp2515
+
+typedef union estados_t {
+  struct {
+    unsigned char bomba1;
+    unsigned char bomba2;
+    //...
+  };
+  unsigned char todas;
+} estados_t;
+
+volatile estados_t estados;
+
+MCP_CAN CAN0(10);                               // Set CS to pin 10
+
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(INTER, INPUT);   // Setting pin A0 for /INT input
+  pinMode(BOMBA1, OUTPUT);
+  pinMode(BOMBA2, OUTPUT);
+  Serial.begin(115200);
+                        
+  while (CAN_OK != CAN0.begin(CAN_125KBPS)) { // init can bus : baudrate = 500k
+    Serial.println("Falha na inicialização");
+    Serial.println("Carregando...");
+    delay(500);
+  }
+  Serial.println("Iniciado com sucesso");
+  estados.todas = 0;
+  //wdt_enable(WDTO_4S); //Função que ativa e altera o Watchdog(_1S _2S _4S _8S)tempos
+}
+
+void loop()
+{
+  //wdt_reset(); //diz que esta tudo ok
+  if (!digitalRead(INTER))                        // If pin A0 is low, read receive buffer
+  {
+    rxId = CAN0.getCanId();
+    if (rxId == 0x000) {
+      CAN0.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
+      estados.todas = *rxBuf;
+      // Get message ID
+      Serial.println("Recebendo");
+    }
+    //wdt_reset(); //diz que esta tudo ok
+    Serial.print("Dados");
+    Serial.println(estados.todas);
+  }
+  digitalWrite(BOMBA1, estados.bomba1);
+  digitalWrite(BOMBA2, estados.bomba2);
+  //wdt_reset(); //diz que esta tudo ok
+}
+
+
---
+++ ModuloInterface_sisAux1/ModuloInterface_sisAux1.ino
@@ -0,0 +1,77 @@
+/*--------------------------------
+--Modulo interface dos sistemas auxiliares, via can bus
+--Autor Natan Ogliari
+--Equipe Zênite solar
+--PD3; PD4; PD5; PD6; PD7;
+*/
+#include "CAN_BUS_Shield/mcp_can.h"
+#include <SPI.h>
+//#include   avr/wdt.h //biblioteca do Watchdog
+//define um nome para as entradas
+#define aciBomba1 3
+#define aciBomba2 4
+#define aciMPPT 5
+#define aux1 6
+#define aux2 7
+
+
+long canId = 0x000;//define o id de modulo
+unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+typedef union estados_t{
+  struct {
+    unsigned char estBomba1;
+    unsigned char estBomba2;
+    unsigned char estMPPT;
+    unsigned char estAux1;
+    unsigned char estAux2;
+    };
+  unsigned char todas;
+} estados_t;
+
+volatile estados_t estados;
+
+MCP_CAN CAN0(10);                               // Set CS to pin 10
+
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  CAN0.begin(CAN_125KBPS);
+  while (CAN_OK != CAN0.begin(CAN_125KBPS)) {
+    Serial.println("Falha na inicialização");
+    Serial.println("Carregando...");
+    delay(500);
+  }
+  Serial.println("Iniciado com sucesso");
+  //wdt_enable(WDTO_4S); //Função que ativa e altera o Watchdog(_1S _2S _4S _8S)tempos
+}
+
+
+void loop()
+{
+  //wdt_reset(); //diz que esta tudo ok
+  estados.todas = 0;
+  Serial.print("Inicio union");
+  Serial.println(estados.todas);
+  estados.estBomba1 = !digitalRead(aciBomba1);
+  estados.estBomba2 = !digitalRead(aciBomba2);
+  estados.estMPPT   = !digitalRead(aciMPPT);
+  estados.estAux1   = !digitalRead(aux1);
+  estados.estAux2   = !digitalRead(aux2);
+  
+  //arruma o vetor a ser enviado 
+
+  Serial.print("Fim union");
+  Serial.println(estados.todas);
+  *stmp = estados.todas;
+   
+  // send data:  id = 0x00, standrad flame, data len = 8, stmp: data buf
+  CAN0.sendMsgBuf(canId, 0, 3, stmp);
+  Serial.print("dado enviado");
+  Serial.println(*stmp);
+  delay(100);
+  //wdt_reset(); //diz que esta tudo ok
+}
+
