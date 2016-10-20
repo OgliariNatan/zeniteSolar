6be020ee80dc0c51fc3a43d09c4b48f31f597fe5
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2016-10-19 22:30:03 -0200

    Update main.c
    "Melhorado o acesso aos dados"

--- MA_sisAux2016___OLD/MA_sisAux2016/main.c
+++ MA_sisAux2016___OLD/MA_sisAux2016/main.c
@@ -1,105 +1,172 @@
-/*
- * MA_sisAux2016.c
+/**
+ * @file main.c
+ * @brief MA_sisAux2016
  *
- * Created: 27/07/2016 23:38:16
- * Author : Natan Ogliari
- * Equipe zênite solar
- */ 
+ * @author Natan Ogliari, João Antônio Cardoso
+ *
+ * @date 10/09/2016
+ */
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
-//#include "avr/wdt.h" //biblioteca whatdog
+
+#include "globalDefines.h"
+#include "ATmega328.h"
 #include "can.h"
-#include <util/delay.h>
 
 #define F_CPU 16000000UL
 
-#define BOMBA1 PD6
-#define BOMBA2 PB0
-#define TensaoBat PD4
+#define ESTADO_INICIAL 0b00000000
+#define BOMBA1		PD6
+#define DDR_BOMBA1	DDRD
+#define PORT_BOMBA1	PORTD
+#define BOMBA2	PB0
+#define DDR_BOMBA2	DDRB
+#define PORT_BOMBA2	PORTB
+#define TensaoBat	PD4
+
+#define MSG_START	0xFD
 
 typedef union estados_t {
 	struct {
-		uint8_t Bomba1;
-		uint8_t Bomba2;
-		uint8_t Inter;
-		uint8_t tensaoBat;
+		uint8_t Bomba1 : 1;
+		uint8_t Bomba2 : 1;
+		//uint8_t tensaoBat;
 	};
-	unsigned char todas;
+	uint8_t all;
 } estados_t;
 
-volatile estados_t estados;
+/**
+ * @brief Seta o filtro do canBus a ser usado
+ *
+ * @param O filtro a ser usado.
+ */
+const uint8_t can_filter[] PROGMEM = {
+	// Group 0
+	MCP2515_FILTER(0),				// Filter 0
+	MCP2515_FILTER(0),				// Filter 1
 
-const uint8_t can_filter[] PROGMEM;
+	// Group 1
+	MCP2515_FILTER(0),				// Filter 2
+	MCP2515_FILTER(0),				// Filter 3
+	MCP2515_FILTER(0),				// Filter 4
+	MCP2515_FILTER(0),				// Filter 5
 
+	MCP2515_FILTER(0),				// Mask 0 (for group 0)
+	MCP2515_FILTER(0),				// Mask 1 (for group 1)
+};
 
-int main(void)
+/**
+ * @brief Configura a CAN
+ */
+void canConfig(void)
 {
-	DDRD |= (1 << BOMBA1); 
-	DDRB |= (1 << BOMBA2);
-	//|| (1 << TensaoBat) configurar adc
-	uint8_t rId = 0;
 	// Initialize MCP2515
 	can_init(BITRATE_125_KBPS);
 
-	
-	can_set_mode(NORMAL_MODE);//!<Modo de operação normal
-	
-	//can_disable_filter(0xff);
-	
 	// Load filters and masks
 	can_static_filter(can_filter);
-	
-	
-	//!<Cria mensagem a ser enviada
-	can_t msg;
-	
-	msg.length = 5;
-	msg.data[0] = 0xde;
-	msg.data[1] = 0xad;
-	msg.data[2] = 0xbe;
-	msg.data[3] = 0xef;
-	msg.data[4] = 0xef;
-	
-	
-	msg.id = 0;
-	msg.flags.rtr = 0;
-	
-		
-	while (1)
-	{	
-		can_send_message(&msg);
-		can_t dado;		
-		if (can_check_message()){//!<Verifica se uma nova mensagem foi recebida
-		
-			if (can_get_message(&dado));//!<Tenta ler a mensagem
-		}
-		
-		estados.Bomba1 = dado.data[0];
-		estados.Bomba2 = dado.data[1];
-		
-		//rId = mcp2515_read_id(dado.id);
-	
-		if (estados.Bomba1) {//!<inverte o estado da bomba1 de porão.
-			PORTD |= (1<<BOMBA1);
-		}else{
-			PORTD &= (0<<BOMBA1);
-		}
-		
-		if (estados.Bomba2) {//!<inverte o estado da bomba2 de porão.
-			PORTD |= (1<<BOMBA2);
-		}else{
-			PORTD &= (0<<BOMBA2);
+
+	// Set normal mode
+	can_set_mode(NORMAL_MODE);
+}
+
+/**
+ * @brief Configura USART
+ *
+void usartSetup(void)
+{
+	// USART Configuration
+	usartConfig(USART_MODE_ASYNCHRONOUS, USART_BAUD_9600, USART_DATA_BITS_8, USART_PARITY_NONE, USART_STOP_BIT_SINGLE);
+	usartEnableReceiver();
+	usartEnableTransmitter();
+	usartClearReceptionBuffer();
+	usartActivateReceptionCompleteInterrupt();
+	usartStdio();
+}
+*/
+/**
+ * @brief Configura as entradas e saídas
+ */
+void ioConfig(void)
+{
+	// set as output
+	setBit(DDR_BOMBA1, BOMBA1);
+	setBit(DDR_BOMBA2, BOMBA2);
+}
+
+/**
+ * @brief Configura todo o sistema
+ */
+void configSystem(void)
+{
+	// Configure CAN
+	canConfig();
+
+	// Configure USART
+	//usartSetup();
+
+	// Configure inputs and outputs
+	ioConfig();
+
+	// Enable global interrupts
+	sei();
+}
+/*
+void usartRead(estados_t *estados, uint8 length)
+{
+	uint8 msg[length];
+	// Enquanto houver dados no buffer
+	// Descarta até encontrar o inicio da mensagem
+	while((!usartIsReceiverBufferEmpty()) && (usartGetDataFromReceiverBuffer() != MSG_START));
+
+	for(uint8 i = 0; i< length; i++){
+		msg[length] = usartGetDataFromReceiverBuffer();
+	}
+
+	(*estados).Bomba1 = msg[0];
+	(*estados).Bomba2 = msg[1];
+}*/
+
+void updateOutputs(estados_t *estados)
+{
+	if( estados->Bomba1 ) setBit(PORT_BOMBA1, BOMBA1);
+	else clrBit(PORT_BOMBA1, BOMBA1);
+
+	if( estados->Bomba2 ) setBit(PORT_BOMBA2, BOMBA2);
+	else clrBit(PORT_BOMBA2, BOMBA2);
+}
+
+/**
+ * @brief Programa principal
+ */
+int main(void)
+{
+	// Configure system
+	configSystem();
+
+	// Initialize system status
+	estados_t estados;
+	estados.all = ESTADO_INICIAL;
+
+	// main loop: reads a message and sends it via USART
+	while (1){
+
+		// Checks if a new message was received
+		if (can_check_message()){
+			can_t received_msg;
+
+			// Try to read the message
+			if(can_get_message(&received_msg)){
+								
+				if ( 0x0030 == received_msg.id){
+					
+					estados.Bomba1 = received_msg.data[0] | 1<<4; //000A B000
+					estados.Bomba2 = received_msg.data[0] | 1<<3;
+					updateOutputs(&estados);
+				}
+			
+			}	
 		}
 	}
 }
-
-const uint8_t can_filter[] PROGMEM = 
-{
-	// Group 0
-	MCP2515_FILTER(0),				// Filter 0
-/*	MCP2515_FILTER(0),				// Filter 1
-	
-	MCP2515_FILTER(0),				// Mask 0 (for group 0)
-	*/
-};
