a4eb35ce52c34dac7eacfa0b1763835f18abf546
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2016-10-27 00:07:13 -0200

    Update main.c

--- MA_sisAux2016___OLD/MA_sisAux2016/main.c
+++ MA_sisAux2016___OLD/MA_sisAux2016/main.c
@@ -14,27 +14,44 @@
 #include "ATmega328.h"
 #include "can.h"
 
+#include "can_defines.h"
+#include "globalMacros.h"
+
 #define F_CPU 16000000UL
 
-#define ESTADO_INICIAL 0b00000000
-#define BOMBA1		PD6
-#define DDR_BOMBA1	DDRD
-#define PORT_BOMBA1	PORTD
-#define BOMBA2	PB0
-#define DDR_BOMBA2	DDRB
-#define PORT_BOMBA2	PORTB
-#define TensaoBat	PD4
+#define BIT_BOMBA1		PD6
+#define DDR_BOMBA1	    DDRD
+#define PORT_BOMBA1	    PORTD
+#define BIT_BOMBA2	    PB0
+#define DDR_BOMBA2	    DDRB
+#define PORT_BOMBA2	    PORTB
+#define BIT_TensaoBat	PD4
+#define DDR_TensaoBat   DDRD
+#define PORT_TensaoBat  PORTD
 
-#define MSG_START	0xFD
+// Local aliases based on the global definitions of can_defines.h
+#define sw_bomba1_msk       data_MI_SisAux2016_A
+#define sw_bomba1_byte      byte_MI_SisAux2016_A
+#define sw_bomba2_msk       data_MI_SisAux2016_B
+#define sw_bomba2_byte      byte_MI_SisAux2016_B
+/*#define sw_mppt_msk         data_MI_SisAux2016_C
+#define sw_mppt_byte        byte_MI_SisAux2016_C
+#define sw_aux1_msk         data_MI_SisAux2016_D
+#define sw_aux1_byte        byte_MI_SisAux2016_D
+#define sw_aux2_msk         data_MI_SisAux2016_E
+#define sw_aux2_byte        byte_MI_SisAux2016_E */
+/* #define on_off_msk          data_MA_SisAux2016_A
+#define on_off_byte         byte_MA_SisAux2016_A
+#define voltage_H_msk       data_MA_SisAux2016_B_H
+#define voltage_H_byte      byte_MA_SisAux2016_B_H
+#define voltage_L_msk       data_MA_SisAux2016_B_L
+#define voltage_L_byte      byte_MA_SisAux2016_B_L */
 
-typedef union estados_t {
-	struct {
-		uint8_t Bomba1 : 1;
-		uint8_t Bomba2 : 1;
-		//uint8_t tensaoBat;
-	};
-	uint8_t all;
-} estados_t;
+typedef struct{
+    uint8_t Bomba1;
+    uint8_t Bomba2;
+    //uint8_t tensaoBat;
+}estados_t;
 
 /**
  * @brief Seta o filtro do canBus a ser usado
@@ -62,7 +79,7 @@ const uint8_t can_filter[] PROGMEM = {
 void canConfig(void)
 {
 	// Initialize MCP2515
-	can_init(BITRATE_125_KBPS);
+	can_init(CAN_BITRATE);
 
 	// Load filters and masks
 	can_static_filter(can_filter);
@@ -71,28 +88,31 @@ void canConfig(void)
 	can_set_mode(NORMAL_MODE);
 }
 
-/*
+/**
  * @brief Configura USART
- *
+ */
 void usartSetup(void)
 {
-	// USART Configuration
-	usartConfig(USART_MODE_ASYNCHRONOUS, USART_BAUD_9600, USART_DATA_BITS_8, USART_PARITY_NONE, USART_STOP_BIT_SINGLE);
+	usartConfig(USART_MODE_ASYNCHRONOUS, 
+                USART_BAUD_9600, 
+                USART_DATA_BITS_8, 
+                USART_PARITY_NONE, 
+                USART_STOP_BIT_SINGLE);
 	usartEnableReceiver();
 	usartEnableTransmitter();
 	usartClearReceptionBuffer();
 	usartActivateReceptionCompleteInterrupt();
 	usartStdio();
 }
-*/
+
 /**
  * @brief Configura as entradas e saídas
  */
 void ioConfig(void)
 {
 	// set as output
-	setBit(DDR_BOMBA1, BOMBA1);
-	setBit(DDR_BOMBA2, BOMBA2);
+	setBit(DDR_BOMBA1, BIT_BOMBA1);
+	setBit(DDR_BOMBA2, BIT_BOMBA2);
 }
 
 /**
@@ -112,32 +132,52 @@ void configSystem(void)
 	// Enable global interrupts
 	sei();
 }
-/*
-void usartRead(estados_t *estados, uint8 length)
-{
-	uint8 msg[length];
-	// Enquanto houver dados no buffer
-	// Descarta até encontrar o inicio da mensagem
-	while((!usartIsReceiverBufferEmpty()) && (usartGetDataFromReceiverBuffer() != MSG_START));
 
-	for(uint8 i = 0; i< length; i++){
-		msg[length] = usartGetDataFromReceiverBuffer();
-	}
-
-	(*estados).Bomba1 = msg[0];
-	(*estados).Bomba2 = msg[1];
-}*/
-
+/**
+ * @brief Atualiza as saidas do sistema de acordo com os estados passados
+ */
 void updateOutputs(estados_t *estados)
 {
-	if( estados->Bomba1 ) setBit(PORT_BOMBA1, BOMBA1);
-	else clrBit(PORT_BOMBA1, BOMBA1);
+	if( estados->Bomba1 ) setBit(PORT_BOMBA1, BIT_BOMBA1);
+	else clrBit(PORT_BOMBA1, BIT_BOMBA1);
 
-	//ATUALIZA_BOMBA1();
-
-	if( estados->Bomba2 ) setBit(PORT_BOMBA2, BOMBA2);
-	else clrBit(PORT_BOMBA2, BOMBA2);
+	if( estados->Bomba2 ) setBit(PORT_BOMBA2, BIT_BOMBA2);
+	else clrBit(PORT_BOMBA2, BIT_BOMBA2);
 }
+
+/**
+ * @brief Desempacota uma mensagem do can e sobre-escreve os estados
+ */
+void readMsg(estados_t *estados)
+{
+    can_t msg;
+
+    // Try to read the message
+    if(can_get_message(msg)){
+								
+        if(msg->id == ID_MI_SisAux2016){
+            // para ler um bit como booleano podemos usar como abaixo:
+            estados->Bomba1 = isTrue( getByte( msg->data, 
+                                                sw_bomba1_byte, 
+                                                sw_bomba1_msk
+                                                ),
+                                      sw_bomba1_msk 
+                                      );
+            estados->Bomba2 = isTrue(   getByte( msg->data,
+                                                sw_bomba2_byte,
+                                                sw_bomba2_msk
+                                                ),
+                                      sw_bomba2_msk 
+                                      );
+
+            /* estados->pot = getByte( msg->data,
+                                    pot_byte,
+                                    pot_msk 
+                                    ); */
+        }
+    }
+}
+ 
 
 /**
  * @brief Programa principal
@@ -152,23 +192,15 @@ int main(void)
 	estados.all = ESTADO_INICIAL;
 
 	// main loop: reads a message and sends it via USART
-	while (1){
+	for(;;){
 
 		// Checks if a new message was received
-		if (can_check_message()){
-			can_t received_msg;
+		if(can_check_message()){
 
-			// Try to read the message
-			if(can_get_message(&received_msg)){
-								
-				if ( 0x000 == received_msg.id){
-					
-					estados.Bomba1  = received_msg.data[0] << 4; //000A B000
-					estados.Bomba2 |= received_msg.data[0] << 3;
-					updateOutputs(&estados);
-				}
+            readMsg(&estados);
+
+            updateOutputs(&estados);
 			
-			}	
 		}
 	}
 }
